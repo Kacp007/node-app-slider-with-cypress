6a9900176ea894a72dd2f3fd640feb1bfd57c91e
Kacper Wegor <kacper.wegor@gmail.com>
2025-03-14 15:48:48 +0100

    tests and hotjar tag

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,25 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+    it('Checks if user can navigate slides using buttons', function () {
+        cy.visit('http://localhost:3000');
+        cy.get('.swiper-button-next').click();
+        cy.wait(2000);
+        cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+        cy.get('.swiper-button-prev').click();
+        cy.wait(2000);
+        cy.get('.swiper-slide-active').should('contain', 'Italy');
+    })
+});
+
+describe('Swiper Gallery Test', function () {
+    it('Checks if gallery is properly displayed', function () {
+        cy.visit('http://localhost:3000');
+        cy.get('.swiper').should('be.visible');
+        cy.get('.swiper-slide').should('have.length', 3);
+        cy.get('.swiper-button-prev').should('be.visible').and('not.be.disabled');
+        cy.get('.swiper-button-next').should('be.visible').and('not.be.disabled');
+    });
+});
